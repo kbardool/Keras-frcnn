21d493f70c4ca9e37a9a9821eacd36dd3935abd3
Kiri Nichol <kiri@winged-delica.telus>
2017-02-05 17:11:14 -0800

    switched parser

--- train_frcnn.py
+++ train_frcnn.py
@@ -14,9 +14,12 @@ sys.setrecursionlimit(40000)
 
 C = config.Config()
 
-import pascal_voc_parser as parser
+#import pascal_voc_parser as parser
+#all_imgs,classes_count,class_mapping = parser.get_data()
 
-all_imgs,classes_count,class_mapping = parser.get_data()
+import parser
+all_imgs,classes_count,class_mapping = parser.get_data('VOCdevkit')
+
 
 with open('classes.json', 'w') as class_data_json:
     json.dump(class_mapping, class_data_json)
@@ -74,13 +77,14 @@ model.summary()
 
 try:
 	if K.image_dim_ordering() == 'th'		:
-		hdf5_filepath = './resnet50_weights_th_dim_ordering_th_kernels_notop.h5'
+		hdf5_filepath = 'resnet50_weights_th_dim_ordering_th_kernels.h5'
 	else:
 		hdf5_filepath = './resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
 except:
 	pass
 
-hdf5_filepath = 'model_frcnn.regr.no_bn.hdf5hdf5'
+#hdf5_filepath = 'model_frcnn.regr.no_bn.hdf5hdf5'
+print 'loading weights from ', hdf5_filepath
 resnet.load_weights_from_hdf5_group_by_name(model,hdf5_filepath)
 #model.load_weights('./model_frcnn.regr.2.hdf5')
 optimizer = Adam(lr = 1e-5)
@@ -94,6 +98,8 @@ nb_epochs = 50
 avg_loss_rpn = []
 avg_loss_class = []
 
+print 'starting training'
+
 for i in range(1,len(train_imgs) * nb_epochs + 1):
 
 	if i%3000 == 0:
@@ -106,7 +112,7 @@ for i in range(1,len(train_imgs) * nb_epochs + 1):
 		for j in range(num_samples_for_val):
 
 			(X1,Y1_class,Y1_regr,X2,Y2) = data_gen_val.next()
-			loss_total,loss_rpn_class,loss_rpn_regr,loss_class = model.train_on_batch([X1,X2],[Y1_class,Y1_regr,Y2])
+			loss_total,loss_rpn_class,loss_rpn_regr,loss_class = model.test_on_batch([X1,X2],[Y1_class,Y1_regr,Y2])
 			val_class_losses += loss_class
 			val_rpn_loss += loss_rpn_class + loss_rpn_regr
 
@@ -118,7 +124,7 @@ for i in range(1,len(train_imgs) * nb_epochs + 1):
 
 		if total_loss < best_val_loss:
 			best_val_loss = total_loss
-			model.save_weights('./model_frcnn.regr.no_bn.2.hdf5hdf5')
+			model.save_weights('./model_frcnn.regr.no_bn.2.hdf5.hdf5')
 
 	(X1,Y1_class,Y1_regr,X2,Y2) = data_gen_train.next()
 
