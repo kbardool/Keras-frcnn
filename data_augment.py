0ad73b1d9850116aefad7843f1eaf343bbab38df
Yann Henon <yannhenon@gmail.com>
2017-02-07 15:22:48 -0500

    added rotation based data augmentation

--- data_augment.py
+++ data_augment.py
@@ -20,6 +20,7 @@ def augment(img_data, config):
 			x2 = bbox['x2']
 			bbox['x2'] = cols - x1
 			bbox['x1'] = cols - x2
+
 	if config.use_vertical_flips and np.random.randint(0, 2) == 0:
 		img = cv2.flip(img, 0)
 		for bbox in img_data['bboxes']:
@@ -28,5 +29,19 @@ def augment(img_data, config):
 			bbox['y2'] = rows - y1
 			bbox['y1'] = rows - y2
 
-	return img_data, img
+	if config.random_rotate:
+		M = cv2.getRotationMatrix2D((cols/2, rows/2), np.random.randint(-config.random_rotate_scale, config.random_rotate_scale), 1)
+		img = cv2.warpAffine(img, M, (cols, rows), flags=cv2.INTER_CUBIC, borderMode= cv2.BORDER_REPLICATE)
+		for bbox in img_data['bboxes']:
+			K = np.array([[bbox['x1'],bbox['y1']],[bbox['x2'],bbox['y2']],[bbox['x1'],bbox['y2']],[bbox['x2'],bbox['y1']]])
+			K = cv2.transform(K.reshape(4,1,2),M)[:,0,:]
 
+			(x1, y1) = np.min(K, axis=0)
+			(x2, y2) = np.max(K, axis=0)
+
+			bbox['x1'] = x1
+			bbox['x2'] = x2
+			bbox['y1'] = y1
+			bbox['y2'] = y2
+
+	return img_data, img
