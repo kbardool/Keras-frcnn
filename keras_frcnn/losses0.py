ca949badd232a8ed725011f8a927c229c2369170
Kiri Nichol <kiri@winged-delica.telus>
2017-02-14 23:03:29 -0800

    borked things

--- keras_frcnn/losses0.py
+++ keras_frcnn/losses0.py
@@ -1,12 +1,9 @@
 from keras import backend as K
-from keras.objectives import categorical_crossentropy
+from keras.objectives import categorical_crossentropy, mean_squared_error
 
 
+lambda_rpn_class = 10.0
 lambda_rpn_regr = 10.0
-lambda_rpn_class = 1.0
-
-lambda_cls_regr = 10.0
-lambda_cls_class = 1.0
 
 
 def rpn_loss_regr(num_anchors):
@@ -25,14 +22,9 @@ def rpn_loss_cls(num_anchors):
 	return rpn_loss_cls_fixed_num
 
 
-def class_loss_regr(num_rois):
-	def class_loss_regr_fixed_num(y_true, y_pred):
-		x = y_true[:, :, 4:] - y_pred
-		x_abs = K.abs(x)
-		x_bool = K.lesser_equal(x_abs, 1.0)
-		return lambda_cls_regr * K.sum(y_true[:, :, :4] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / num_rois
-	return class_loss_regr_fixed_num
+def class_loss_regr(y_true, y_pred):
+	return mean_squared_error(y_true, y_pred)
 
 
 def class_loss_cls(y_true, y_pred):
-	return lambda_cls_class * categorical_crossentropy(y_true, y_pred)
+	return categorical_crossentropy(y_true, y_pred)
