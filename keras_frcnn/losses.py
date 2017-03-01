f27a3aa5ab19575e96894e01836cd0ac97466233
Yann Henon <yannhenon@gmail.com>
2017-03-01 15:19:29 -0500

    updated for new fixes

--- keras_frcnn/losses.py
+++ keras_frcnn/losses.py
@@ -8,6 +8,7 @@ lambda_rpn_class = 1.0
 lambda_cls_regr = 10.0
 lambda_cls_class = 1.0
 
+epsilon = 1e-5
 
 def rpn_loss_regr(num_anchors):
 	def rpn_loss_regr_fixed_num(y_true, y_pred):
@@ -15,13 +16,13 @@ def rpn_loss_regr(num_anchors):
 		x_abs = K.abs(x)
 		x_bool = K.lesser_equal(x_abs, 1.0)
 		return lambda_rpn_regr * K.sum(
-			y_true[:, :4 * num_anchors, :, :] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / 256.
+			y_true[:, :4 * num_anchors, :, :] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / K.sum(epsilon + y_true[:, :4 * num_anchors, :, :])
 	return rpn_loss_regr_fixed_num
 
 
 def rpn_loss_cls(num_anchors):
 	def rpn_loss_cls_fixed_num(y_true, y_pred):
-		return lambda_rpn_class * K.sum(y_true[:, :num_anchors, :, :] * K.binary_crossentropy(y_pred[:, :, :, :], y_true[:, num_anchors:, :, :])) / 256.0
+		return lambda_rpn_class * K.sum(y_true[:, :num_anchors, :, :] * K.binary_crossentropy(y_pred[:, :, :, :], y_true[:, num_anchors:, :, :])) / K.sum(epsilon + y_true[:, :num_anchors, :, :])
 	return rpn_loss_cls_fixed_num
 
 
@@ -30,9 +31,9 @@ def class_loss_regr(num_rois,num_classes):
 		x = y_true[:, :, 4*num_classes:] - y_pred
 		x_abs = K.abs(x)
 		x_bool = K.lesser_equal(x_abs, 1.0)
-		return lambda_cls_regr * K.sum(y_true[:, :, :4*num_classes] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / num_rois
+		#return K.mean(0 * y_true ) + K.mean(0*y_pred)
+		return lambda_cls_regr * K.sum(y_true[:, :, :4*num_classes] * (x_bool * (0.5 * x * x) + (1 - x_bool) * (x_abs - 0.5))) / K.sum(y_true[:, :, :4*num_classes])
 	return class_loss_regr_fixed_num
-
 
 def class_loss_cls(y_true, y_pred):
 	return lambda_cls_class * categorical_crossentropy(y_true, y_pred)
--- test_frcnn.py
+++ test_frcnn.py
@@ -23,15 +23,18 @@ def format_img(img):
 	if width <= height:
 		f = img_min_side/width
 		new_height = int(f * height)
-		new_width = 600
+		new_width = int(img_min_side)
 	else:
 		f = img_min_side/height
 		new_width = int(f * width)
-		new_height = 600
+		new_height = int(img_min_side)
 	img = cv2.resize(img,(new_width,new_height),interpolation = cv2.INTER_CUBIC)
+	img = img[:,:,(2,1,0)]
 	img = np.transpose(img,(2,0,1)).astype(np.float32)
 	img = np.expand_dims(img, axis=0)
-	img -= 127.5
+	img[:, 0, :, :] -= 103.939
+	img[:, 1, :, :] -= 116.779
+	img[:, 2, :, :] -= 123.68
 	return img
 
 with open('classes.json', 'r') as class_data_json:
@@ -80,8 +83,9 @@ classifier = nn.classifier(feature_map_input,roi_input,num_rois,nb_classes=len(c
 model_rpn = Model(img_input,rpn + [shared_layers])
 model_classifier = Model([feature_map_input,roi_input],classifier)
 
-model_rpn.load_weights('model_frcnn.hdf5', by_name=True)
-model_classifier.load_weights('model_frcnn.hdf5', by_name=True)
+weights_path = 'model_frcnn.hdf5'
+model_rpn.load_weights(weights_path, by_name=True)
+model_classifier.load_weights(weights_path, by_name=True)
 
 model_rpn.compile(optimizer='sgd',loss='mse')
 model_classifier.compile(optimizer='sgd',loss='mse')
@@ -100,8 +104,15 @@ for idx,img_name in enumerate(sorted(os.listdir(img_path))):
 	print(img_name)
 	filepath = os.path.join(img_path,img_name)
 	img = cv2.imread(filepath)
+
 	X = format_img(img)
-	img_scaled = (np.transpose(X[0,:,:,:],(1,2,0)) + 127.5).astype('uint8')
+
+	img_scaled = np.transpose(X[0,(2,1,0),:,:],(1,2,0)).copy()
+	img_scaled[:, :, 0] += 124
+	img_scaled[:, :, 1] += 117
+	img_scaled[:, :, 2] += 104
+	img_scaled = img_scaled.astype(np.uint8)
+
 	# get the feature maps and output from the RPN
 	[Y1,Y2,F] = model_rpn.predict(X)
 
@@ -128,11 +139,12 @@ for idx,img_name in enumerate(sorted(os.listdir(img_path))):
 			ROIs_padded[0,curr_shape[1]:,:] = ROIs[0,0,:]
 			ROIs = ROIs_padded
 
-		[P_cls,P_regr] = model_classifier.predict([F,ROIs])
+		[P_cls, P_regr] = model_classifier.predict([F, ROIs])
+		P_regr = P_regr / C.std_scaling
 
 		for ii in range(P_cls.shape[1]):
 
-			if np.max(P_cls[0,ii,:]) < 0.8 or np.argmax(P_cls[0,ii,:]) == (P_cls.shape[2] - 1):
+			if np.max(P_cls[0,ii,:]) < 0.1 or np.argmax(P_cls[0,ii,:]) == (P_cls.shape[2] - 1):
 				continue
 
 			cls_name = class_mapping[np.argmax(P_cls[0,ii,:])]
@@ -141,14 +153,14 @@ for idx,img_name in enumerate(sorted(os.listdir(img_path))):
 				bboxes[cls_name] = []
 				probs[cls_name] = []
 
-			(x,y,w,h) = ROIs[0,ii,:]
+			(x, y, w, h) = ROIs[0,ii,:]
 
-			cls_num = np.argmax(P_cls[0,ii,:])
-			(tx,ty,tw,th) = P_regr[0,ii,4*cls_num:4*(cls_num+1)]
-			x,y,w,h = roi_helpers.apply_regr(x,y,w,h,tx,ty,tw,th)
+			cls_num = np.argmax(P_cls[0, ii, :])
+			(tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
+			x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
 
 			bboxes[cls_name].append([16*x,16*y,16*(x+w),16*(y+h)])
-			probs[cls_name].append(np.max(P_cls[0,ii,:]))
+			probs[cls_name].append(np.max(P_cls[0, ii, :]))
 
 	all_dets = {}
 
@@ -157,10 +169,11 @@ for idx,img_name in enumerate(sorted(os.listdir(img_path))):
 		print(len(bboxes[key]))
 		bbox = np.array(bboxes[key])
 
-		new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlapThresh = 0.5)
+		new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlapThresh=0.5)
 		for jk in range(new_boxes.shape[0]):
 			(x1,y1,x2,y2) = new_boxes[jk,:]
-			cv2.rectangle(img_scaled,(x1,y1),(x2,y2),class_to_color[key],2)
+
+			cv2.rectangle(img_scaled,(x1, y1), (x2, y2), class_to_color[key],2)
 
 			textLabel = '{}:{}'.format(key,int(100*new_probs[jk]))
 			if key not in all_dets:
@@ -171,8 +184,8 @@ for idx,img_name in enumerate(sorted(os.listdir(img_path))):
 			(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
 			textOrg = (x1,y1+20)
 
-			cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(0,0,0),2)
-			cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(255,255,255),-1)
-			cv2.putText(img_scaled,textLabel,textOrg,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),1)
+			#cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(0,0,0),2)
+			#cv2.rectangle(img_scaled,(textOrg[0] - 5,textOrg[1]+baseLine - 5),(textOrg[0]+retval[0] + 5,textOrg[1]-retval[1] - 5),(255,255,255),-1)
+			#cv2.putText(img_scaled,textLabel,textOrg,cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,0),1)
 	cv2.imshow('img',img_scaled)
 	cv2.waitKey(0)
