55016ecd0e635ab214070d5fd2166c74e271459e
yrrah <yrrah7@gmail.com>
2020-08-31 18:43:02 -0400

    Update roi_helpers.py
    update for Keras compatibility

--- keras_frcnn/roi_helpers.py
+++ keras_frcnn/roi_helpers.py
@@ -229,16 +229,16 @@ def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=
 
 	assert rpn_layer.shape[0] == 1
 
-	if dim_ordering == 'th':
+	if dim_ordering == 'channels_first':
 		(rows,cols) = rpn_layer.shape[2:]
 
-	elif dim_ordering == 'tf':
+	elif dim_ordering == 'channels_last':
 		(rows, cols) = rpn_layer.shape[1:3]
 
 	curr_layer = 0
-	if dim_ordering == 'tf':
+	if dim_ordering == 'channels_first':
 		A = np.zeros((4, rpn_layer.shape[1], rpn_layer.shape[2], rpn_layer.shape[3]))
-	elif dim_ordering == 'th':
+	elif dim_ordering == 'channels_first':
 		A = np.zeros((4, rpn_layer.shape[2], rpn_layer.shape[3], rpn_layer.shape[1]))
 
 	for anchor_size in anchor_sizes:
@@ -246,7 +246,7 @@ def rpn_to_roi(rpn_layer, regr_layer, C, dim_ordering, use_regr=True, max_boxes=
 
 			anchor_x = (anchor_size * anchor_ratio[0])/C.rpn_stride
 			anchor_y = (anchor_size * anchor_ratio[1])/C.rpn_stride
-			if dim_ordering == 'th':
+			if dim_ordering == 'channels_first':
 				regr = regr_layer[0, 4 * curr_layer:4 * curr_layer + 4, :, :]
 			else:
 				regr = regr_layer[0, :, :, 4 * curr_layer:4 * curr_layer + 4]
